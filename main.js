e6aa110a608cd890cc81b86a8039138eb0a6de1f
Ailen Fleites <ailenfleites3@gmail.com>
2021-01-16 12:56:10 -0300

    3rd commit

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const urlBase = 'https://5ffb628063ea2f0017bdb0ef.mockapi.io/'
 
+const getEmployees = () => {
+    const tbody = document.getElementById('data-employees');
 
-fetch(urlBase + '/users')
-.then( response => response.json())
-.then (data => console.log(data));
+    fetch(urlBase + 'users')
+        .then(response => response.json())
+        .then(data => {
+            const employees = data;
+             for (const employee of employees) {
+                const tr = document.createElement('tr');
+                tr.innerHTML= `
+                <td> ${employee.fullname} </td>
+                <td> ${employee.address} </td>
+                <td> ${employee.phone} </td>
+                <td> ${employee.email} </td>
+                <td><i class="material-icons" id="${employee.id}"  title="Edit">&#xE254;</i>
+                <i class="material-icons" id="${employee.id}" title="Delete">&#xE872;</i></td>`;
+                tbody.appendChild(tr);
+             }
+        }
+    );
+}
+getEmployees();
+
